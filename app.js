33e2c760593a74ab3ed64fe4712662ac12ba1792
Ezio <15624361+Ezoio@users.noreply.github.com>
2018-12-27 21:04:45 +0800

    cloud support

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
+    })
+    
+    wx.cloud.init({
+      traceUser:true
     })
   },
   globalData: {
